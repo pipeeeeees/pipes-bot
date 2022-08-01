de5fcfb75bcf575df705e64a027d64b80bc45f2b
Pipes, David Henry Jr <48690614+pipeeeeees@users.noreply.github.com>
2022-08-01 18:24:06 -0400

    Update meme_selector.py

--- meme_selector.py
+++ meme_selector.py
@@ -2,15 +2,14 @@ import os
 import random
 
 class MemeFolder:
-    CURRENT_WORKING_DIR = os.getcwd()
     def __init__(self, folder_name):
-        self.self_list = os.listdir(CURRENT_WORKING_DIR + '\\' + folder_name)
+        self.self_list = os.listdir(os.getcwd() + '\\' + folder_name)
         self.self_list_len = len(self.self_list)
         self.index = 0
         random.shuffle(self.self_list)
         
     def return_path(self):
-        path_return = CURRENT_WORKING_DIR + '\\' + self.self_list[self.index]
+        path_return = os.getcwd() + '\\' + self.self_list[self.index]
         self.index += 1
         print(path_return)
         return path_return

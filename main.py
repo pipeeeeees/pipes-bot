c21cef9f666482ddbd9bf3d9e04f57d6aa74565a
Pipes, David Henry Jr <48690614+pipeeeeees@users.noreply.github.com>
2022-08-03 23:04:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -134,12 +134,13 @@ async def on_message(message):
   
     if message.content.startswith('$spotify '):
         keyword = str(message.content).replace('$spotify ','')
-        try:
-            mystring = f"""You have requested to search Spotify for playlists containing the keyword '{keyword}'. I will return the top songs that appear the most in those playlists. Please wait while I retrieve that information...\n"""
-            await message.channel.send(mystring)
-            await message.channel.send(spotify_search.popular_tracks_based_on_keyword(keyword))
-        except:
-            await message.channel.send('An error occurred. Please try again.')
+        
+        #try:
+        mystring = f"""You have requested to search Spotify for playlists containing the keyword '{keyword}'. I will return the top songs that appear the most in those playlists. Please wait while I retrieve that information...\n"""
+        await message.channel.send(mystring)
+        await message.channel.send(spotify_search.popular_tracks_based_on_keyword(keyword))
+        #except:
+            #await message.channel.send('An error occurred. Please try again.')
   
     if message.content.startswith('$pollen'):
         try:

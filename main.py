e4ec19e7ed1a7dd50e270f0395a402504df54095
David Pipes <48690614+pipeeeeees@users.noreply.github.com>
2022-09-29 22:06:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -157,7 +157,7 @@ async def on_message(message):
     elif message.content.startswith('$spotify-'):
         try:
             numtimes = int(str(message.content).replace("$spotify-","").split(" ")[0])
-            keyword = str(message.content).split(" ")[2]
+            keyword = str(message.content).split(" ")[1]
             
             #try:
             mystring = f"""You have requested to search Spotify for playlists containing the keyword '{keyword}'. I will return the top songs that appear the most in those playlists. Please wait while I retrieve that information...\n"""

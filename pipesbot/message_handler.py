79f731ee7644d5a77e0e09bcdec639e123589f4b
pipeeeeees <48690614+pipeeeeees@users.noreply.github.com>
2023-12-06 20:38:19 -0500

    Update message_handler.py

--- pipesbot/message_handler.py
+++ pipesbot/message_handler.py
@@ -79,10 +79,14 @@ async def handler(client, message):
             gas_prices = pd.read_pickle(r'pipesbot\pickles\gas_prices_ga.pkl')
             await dm_channel.send(f'```{gas_prices}```')
         else:
-            await dm_channel.send(f'No gas prices pickle file exists.')
-            # send us a list of files and directories within pipesbot/ in the cwd
-            list_of_files = os.listdir(os.getcwd() + '/pipesbot/pickles')
-            await dm_channel.send(f'```{list_of_files}```')
+            try:
+                gas_prices = pd.read_pickle(r'pipesbot\pickles\gas_prices_ga.pkl')
+                await dm_channel.send(f'```{gas_prices}```')
+            except:
+                await dm_channel.send(f'No gas prices pickle file exists.')
+                # send us a list of files and directories within pipesbot/ in the cwd
+                list_of_files = os.listdir(os.getcwd() + '/pipesbot/pickles/gas_prices_ga.pkl')
+                await dm_channel.send(f'```{list_of_files}```')
         return
     if message.content.startswith('pipesbot,'): #GPT reply
         msg = message.content.replace('pipesbot,','')

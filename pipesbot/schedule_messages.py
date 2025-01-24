80cfbd88181c3a69ced0fe8ea1599c634fe86602
David Pipes <pipeeeeees@gmail.com>
2025-01-24 00:57:13 -0500

    Update schedule_messages.py

--- pipesbot/schedule_messages.py
+++ pipesbot/schedule_messages.py
@@ -269,9 +269,9 @@ def morning_report_message(plot=False):
 
         # SUNSET
         sunset = weather.get_sun_time(0, "sunset")
-        sunset_string = f"\n- Sunset is at {sunset}."
+        sunset_string = f"\n- Sunset is at {sunset} 🌅"
         sunrise = weather.get_sun_time(1, "sunrise")
-        sunrise_string = f"\n- Sunrise tomorrow is at {sunrise}."
+        sunrise_string = f"\n- Sunrise tomorrow is at {sunrise} 🌅"
         message_string = message_string + sunset_string
         message_string = message_string + sunrise_string
 
